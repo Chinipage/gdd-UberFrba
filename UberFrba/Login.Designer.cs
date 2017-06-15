c29bb5e1d73283e4d5cd08b147303d0a598213f3
nicosoutn2014 <nicolasmp920@gmail.com>
2017-06-15 20:10:54 -0300

    repusheo el proyecto salvado

---
+++ UberFrba/Login.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace UberFrba
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(122, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Usuario";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(131, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 26);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Clave";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(35, 68);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(278, 26);
+            this.txtUser.TabIndex = 2;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(35, 157);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(278, 26);
+            this.txtPass.TabIndex = 3;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(123, 210);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(95, 36);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(348, 264);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login | UberFrba";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Button btnLogin;
+    }
+}
---
+++ UberFrba/Login.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Security.Cryptography;
+
+namespace UberFrba
+{
+    public partial class Login : Form
+    {
+        public int loginFailAttempts = 0;
+        public string connectionString = ConfigurationManager.AppSettings["connString"];
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+           
+            txtUser.Focus();
+            txtPass.PasswordChar = '*';
+        }
+
+        //Método que loguea al usuario
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            //El sistema chequea si están ambos campos completos
+            if (txtUser.Text.Length == 0 || txtPass.Text.Length == 0)
+            {
+                MessageBox.Show("[WARNING] Por favor escriba Usuario y Contraseña");
+                return;
+            }
+
+            //El sistema chequea si la pass es correcta
+            if (checkPassOK())
+            {
+                checkProfile();
+            }
+            else
+            {
+                //Si no es correcta el sistema incrementa los intentos fallidos
+                loginFailAttempts++;
+                //Si superó los intentos fallidos el sistema bloquea el usuario
+                if (loginFailAttempts == 3)
+                {
+                    bool flag = blockUser(txtUser.Text);
+                    if (flag)
+                    {
+                        MessageBox.Show("[ERROR] Usuario o clave incorrectos! Su Usuario ha sido deshabilitado");
+                        btnLogin.Enabled = false;  
+                    }
+                    else
+                    {
+                        MessageBox.Show("[ERROR] Error al bloquear el Usuario");
+                        return;
+                    }
+                }
+                else
+                    MessageBox.Show("[ERROR] Usuario o clave incorrectos! Vuelva a intentarlo. Intentos Restantes " + (3 - loginFailAttempts).ToString());
+                return;
+            }
+        }
+
+        //Funcion que verifica si la contraseña es correcta
+        private bool checkPassOK()
+        {
+            string query = "";
+
+            using (var conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    query = string.Format(@"select USUA_HABILITADO
+                                            from GESTION_DE_GATOS.USUARIO
+                                            where USUA_CONTRASENIA = (GESTION_DE_GATOS.encriptar_contrasenia('{0}'))
+                                            and USUA_USERNAME = '{1}'",
+                                            txtPass.Text, txtUser.Text);
+                    SqlCommand cmmd = new SqlCommand(query, conn);
+                    conn.Open();
+                    SqlDataReader reg = null;
+                    reg = cmmd.ExecuteReader();
+                    if (reg.Read())
+                    {
+                        if ((bool)reg["USUA_HABILITADO"])
+                        {
+                            conn.Close();
+                            return true;
+                        }
+                        else
+                        {
+                            MessageBox.Show("El usuario " + txtUser.Text + " se encuentra deshabilitado para utilizar el Sistema");
+                            conn.Close();
+                            Application.Exit();
+                        }
+                    }
+                    else
+                        return false;
+                }
+                catch (SqlException sqlEx)
+                {
+                    MessageBox.Show(sqlEx.Message);
+                }
+            }
+            return true;
+        }
+
+        //Funcion que bloquea el usuario luego de 3 intentos fallidos
+        private bool blockUser(string user)
+        {
+            string query = "";
+            using (var conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    query = string.Format(@"update a
+                                            set USUA_HABILITADO = 0
+                                            from GESTION_DE_GATOS.USUARIO as a
+                                            where USUA_USERNAME = '{0}'", user);
+                    SqlCommand cmmd = new SqlCommand(query, conn);
+                    conn.Open();
+                    int rows = cmmd.ExecuteNonQuery();
+                    conn.Close();
+                    if (rows > 0)
+                        return true;
+                    else
+                    {
+                        MessageBox.Show("El usuario no existe. No se puede bloquear");
+                        return false;
+                    }
+                }
+                catch (SqlException sqlEx)
+                {
+                    MessageBox.Show(sqlEx.Message);
+                    return false;
+                }
+            }
+        }
+
+        private void checkProfile()
+        {
+            string query = "";
+            using (var conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    query = string.Format(@"select ROL_ID, ROL_DESCRIPCION, ROL_HABILITADO, USUA_USERNAME
+                                            from GESTION_DE_GATOS.ROL_USUARIO as a
+                                            inner join GESTION_DE_GATOS.ROL as c on c.ROL_ID = a.RU_ROL_ID
+                                            inner join GESTION_DE_GATOS.USUARIO as b on a.RU_USUA_ID = b.USUA_ID
+                                            where USUA_USERNAME = '{0}' and ROL_HABILITADO = 1", txtUser.Text);
+                    SqlCommand cmmd = new SqlCommand(query, conn);
+                    DataTable dt = new DataTable();
+                    conn.Open();
+                    dt.Load(cmmd.ExecuteReader());
+                    conn.Close();
+                    //El sistema chequea que el usuario tegna algun Rol asignado
+                    if (dt.Rows.Count > 0)
+                    {
+                        //Si tiene más de 1 Rol se le pide al usuario seleccionar con cual quiere loguearse
+                        if (dt.Rows.Count > 1)
+                        {
+                            RolSelection frmRol = new RolSelection(txtUser.Text);
+                            frmRol.Show();
+                            this.Hide();
+                        }
+                        else
+                        {
+                            Form1 frm = new Form1(txtUser.Text, dt.Rows[0]["ROL_ID"].ToString());
+                            frm.Show();
+                            //this.Close();
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("[WARNING] El usuario no tiene ningún Rol asignado o su/s Rol/es está/n deshabilitado/s.");
+                    }
+                }
+                catch (SqlException sqlEx)
+                {
+                    MessageBox.Show(sqlEx.Message);
+                    return;
+                }
+            }
+        }
+    }
+}
