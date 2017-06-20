10e8c4a2eed4108d701c443d70e67255a6505ee6
nicosoutn2014 <nicolasmp920@gmail.com>
2017-06-20 01:14:59 -0300

    Se corrigio una pavada en Registrar Viaje

--- UberFrba/Registro Viajes/Form8.cs
+++ UberFrba/Registro Viajes/Form8.cs
@@ -54,23 +54,14 @@ namespace UberFrba.Registro_Viajes
 
                     //El sistema obtiene los choferes, turnos y clientes habilitados
                     string queryChof = "select ((convert(nvarchar(8), CHOF_DNI)) + ' | ' + CHOF_NOMBRE + ' ' + CHOF_APELLIDO) as CHOFER, CHOF_ID from GESTION_DE_GATOS.CHOFER where CHOF_HABILITADO = 1";
-                    string queryTurnos = "select distinct(TURN_DESCRIPCION), TURN_ID from GESTION_DE_GATOS.TURNO where TURN_HABILITADO = 1";
                     string queryClie = "select CLIE_ID, (convert(nvarchar(8), CLIE_DNI) + ' | ' + CLIE_NOMBRE + ' ' + CLIE_APELLIDO) as CLIENTE from GESTION_DE_GATOS.CLIENTE where CLIE_HABILITADO = 1";
                     SqlDataAdapter da = new SqlDataAdapter(queryChof, conn);
-                    SqlDataAdapter da2 = new SqlDataAdapter(queryTurnos, conn);
                     SqlDataAdapter da3 = new SqlDataAdapter(queryClie, conn);
                     conn.Open();
                     DataSet ds = new DataSet();
                     da.Fill(ds, "Choferes");
-                    da2.Fill(ds, "Turnos");
                     da3.Fill(ds, "Clientes");
                     conn.Close();
-
-                    //El sistema llena el combo de Turnos
-                    comboTurno.DataSource = ds.Tables["Turnos"];
-                    comboTurno.DisplayMember = "TURN_DESCRIPCION";
-                    comboTurno.ValueMember = "TURN_ID";
-                    comboTurno.DropDownStyle = ComboBoxStyle.DropDownList;
 
                     //El sistema llena el combo de Choferes
                     comboChof.DataSource = ds.Tables["Choferes"];
@@ -104,8 +95,8 @@ namespace UberFrba.Registro_Viajes
                     SqlCommand cmmd = new SqlCommand("GESTION_DE_GATOS.p_get_turno_vehiculo_de_viaje", conn);
                     cmmd.CommandType = System.Data.CommandType.StoredProcedure;
                     SqlParameter param_chof = new SqlParameter("@CHOFER", int.Parse(comboChof.SelectedValue.ToString()));
-                    SqlParameter param_fec_ini = new SqlParameter("@FECHA_INICIO", dateTimePickerIni.Value.Date.ToShortDateString() + " " + dateTimePickerIni.Value.Hour.ToString() + ":00:00");
-                    SqlParameter param_fec_fin = new SqlParameter("@FECHA_FIN", dateTimePickerFin.Value.Date.ToShortDateString() + " " + dateTimePickerFin.Value.Hour.ToString() + ":00:00");
+                    SqlParameter param_fec_ini = new SqlParameter("@VIAJE_HORA_INICIO", dateTimePickerIni.Value.Date.ToShortDateString() + " " + dateTimePickerIni.Value.Hour.ToString() + ":00:00");
+                    SqlParameter param_fec_fin = new SqlParameter("@VIAJE_HORA_FIN", dateTimePickerFin.Value.Date.ToShortDateString() + " " + dateTimePickerFin.Value.Hour.ToString() + ":00:00");
                     param_chof.Direction = ParameterDirection.Input;
                     param_fec_ini.Direction = ParameterDirection.Input;
                     param_fec_fin.Direction = ParameterDirection.Input;
